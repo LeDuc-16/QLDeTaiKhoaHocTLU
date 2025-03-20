20209dadd5135f294a28a3b4cc1f5608a5c280a3
Minhtrung2004 <ngominhtrung08052004@gmail.com>
2025-03-20 14:56:57 +0700

    oke

--- src/components/header/UserDropdown.tsx
+++ src/components/header/UserDropdown.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { DropdownItem } from "../ui/dropdown/DropdownItem";
 import { Dropdown } from "../ui/dropdown/Dropdown";
 import { Link } from "react-router";
-
 export default function UserDropdown() {
   const [isOpen, setIsOpen] = useState(false);
 
--- src/pages/Dashboard/Home.tsx
+++ src/pages/Dashboard/Home.tsx
@@ -1,10 +1,13 @@
 import { FaGraduationCap, FaBook, FaChartBar, FaUserGraduate, FaClipboardList } from "react-icons/fa";
-
+import { useNavigate } from "react-router-dom";
 export default function Home() {
-  // Hàm Card đơn giản, nhận label và icon
-  const Card = ({ label, icon }: { label: string; icon: React.ReactNode }) => {
+  const navigate = useNavigate()
+  const Card = ({ label, icon, onClick }: { label: string; icon: React.ReactNode; onClick: () => void; }) => {
     return (
-      <div className="bg-blue-400 rounded-lg p-6 flex flex-col items-center justify-center h-60  hover:bg-blue-800 hover:shadow-lg transition-shadow">
+      <div
+        className="bg-blue-500 rounded-lg p-6 flex flex-col items-center justify-center h-60 hover:bg-blue-800 hover:shadow-lg transition-shadow cursor-pointer" // Thêm cursor-pointer
+        onClick={onClick}
+      >
         <div className="text-white text-4xl mb-2">{icon}</div>
         <p className="text-white text-lg font-medium">{label}</p>
       </div>
@@ -16,26 +19,40 @@ export default function Home() {
       {/* Hàng trên: 3 card */}
       <div className="col-span-12 grid grid-cols-3 gap-4 md:gap-6">
         <div className="col-span-1">
-          <Card label="Cổng bảo khoa học" icon={<FaGraduationCap />} />
+          <Card
+            label="Giảng viên"
+            icon={<FaGraduationCap />}
+            onClick={() => navigate('/basic-tables')}
+          />
         </div>
         <div className="col-span-1">
-          <Card label="Tập chí, ấn phẩm" icon={<FaBook />} />
+          <Card
+            label="Đề tài nghiên cứu"
+            icon={<FaBook />}
+            onClick={() => navigate('')}
+          />
         </div>
         <div className="col-span-1">
-          <Card label="Đề tài nghiên cứu" icon={<FaChartBar />} />
-        </div>
-      </div>
-
-      {/* Hàng dưới: 2 card so le, căn giữa 2 khe */}
-      <div className="col-span-12 grid grid-cols-4 gap-4 md:gap-2">
-        <div className="col-span-1 col-start-1"></div> {/* Khoảng trống bên trái */}
-        <div className="col-span-1">
-          <Card label="Giảng viên" icon={<FaUserGraduate />} />
+          <Card
+            label="Công bố khoa học"
+            icon={<FaUserGraduate />}
+            onClick={() => navigate('/science-tables')}
+          />
         </div>
         <div className="col-span-1">
-          <Card label="Báo cáo - thống kê" icon={<FaClipboardList />} />
+          <Card
+            label="Tập chí, ấn phẩm"
+            icon={<FaClipboardList />}
+            onClick={() => navigate('/an-pham')}
+          />
         </div>
-        <div className="col-span-1"></div> {/* Khoảng trống bên phải */}
+        <div className="col-span-1">
+          <Card
+            label="Báo cáo - thống kê"
+            icon={<FaChartBar />}
+            onClick={() => navigate('')}
+          />
+        </div>
       </div>
     </div>
   );
